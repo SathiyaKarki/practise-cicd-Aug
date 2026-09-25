5b007563d9fce75ed8f08c8567be0b7ec6750b70
SathiyaKarki <63051380+SathiyaKarki@users.noreply.github.com>
2026-09-25 11:26:51 +0530

    Update server.js

--- AWS/my-beanstalk-app/server.js
+++ AWS/my-beanstalk-app/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8080;
 // Define a simple route for the root URL
 app.get('/', (req, res) => {
   console.log('Received a request!');
-  res.send('<h1>Thisis an Elastic bean stalk application </h1>');
+  res.send('<h1>This is an Elastic bean stalk application </h1>');
 });
 
 // Start the server and listen on the specified port
